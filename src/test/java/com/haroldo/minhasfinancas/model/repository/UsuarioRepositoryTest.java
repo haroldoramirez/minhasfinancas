56194410687f964544e118900f672c62b4715416
Haroldo Ramirez da Nobrega <haroldoramirez@gmail.com>
2022-05-11 15:11:45 -0300

    Implementado testes de integracao no usuario repository

--- src/test/java/com/haroldo/minhasfinancas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/haroldo/minhasfinancas/model/repository/UsuarioRepositoryTest.java
@@ -5,26 +5,42 @@ import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import java.util.Optional;
+
 @ActiveProfiles("test")
 @RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UsuarioRepositoryTest {
 	
 	//Teste de Integracao
 	
 	@Autowired
 	UsuarioRepository repository;
+
+	@Autowired
+	TestEntityManager entityManager;
+
+	public static Usuario criarUsuario() {
+		return Usuario.builder()
+				.nome("usuario")
+				.email("usuario@email.com")
+				.senha("senha")
+				.build();
+	}
 	
 	@Test
 	public void deveVerificarAExistenciaDeUmEmail() {
 		
 		//Cenario
-		Usuario usuario = Usuario.builder().nome("usuario").email("usuario@email.com").build();
-		repository.save(usuario);
+		Usuario usuario = criarUsuario();
+		entityManager.persist(usuario);
 		
 		//Acao - Execucao
 		boolean result = repository.existsByEmail("usuario@email.com");
@@ -36,16 +52,51 @@ public class UsuarioRepositoryTest {
 	
 	@Test
 	public void deveRetornarFalsoQuandoNaoHouverUsuarioCadastradoComEmail() {
-		
-		//Cenario
-		repository.deleteAll();
-		
+
 		//Acao - Execucao
 		boolean result = repository.existsByEmail("usuario@email.com");
 		
 		//Verificacao
-		
 		Assertions.assertThat(result).isFalse();
+
+	}
+
+	@Test
+	public void devePersistirUmUsuarioNaBaseDeDados() {
+
+		//Cenario
+		Usuario usuario = criarUsuario();
+
+		//Acao
+		Usuario usuarioSalvo = repository.save(usuario);
+
+		//Verificacao
+		Assertions.assertThat(usuarioSalvo.getId()).isNotNull();
+
+	}
+
+	@Test
+	public void deveBuscarUmUsuarioPorEmail() {
+
+		//Cenario
+		Usuario usuario = criarUsuario();
+		entityManager.persist(usuario);
+
+		//Verificacao
+		Optional<Usuario> result = repository.findByEmail("usuario@email.com");
+
+		Assertions.assertThat(result.isPresent()).isTrue();
+
+	}
+
+	@Test
+	public void deveRetornarVazioAoBuscarUsuarioPorEmailQuandoNaoExisteNaBase() {
+
+		//Verificacao
+		Optional<Usuario> result = repository.findByEmail("usuario@email.com");
+
+		Assertions.assertThat(result.isPresent()).isFalse();
+
 	}
 
 }
