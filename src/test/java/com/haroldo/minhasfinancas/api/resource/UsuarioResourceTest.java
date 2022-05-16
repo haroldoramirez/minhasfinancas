89bdb8d0b11e64c96239d368f25256bf625243bf
Haroldo Ramirez da Nobrega <haroldoramirez@gmail.com>
2022-05-16 11:21:36 -0300

    Concluido todos os testes

--- src/test/java/com/haroldo/minhasfinancas/api/resource/UsuarioResourceTest.java
+++ src/test/java/com/haroldo/minhasfinancas/api/resource/UsuarioResourceTest.java
@@ -2,6 +2,8 @@ package com.haroldo.minhasfinancas.api.resource;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.haroldo.minhasfinancas.api.dto.UsuarioDTO;
+import com.haroldo.minhasfinancas.exception.ErroAutenticacaoException;
+import com.haroldo.minhasfinancas.exception.RegraNegocioException;
 import com.haroldo.minhasfinancas.model.entity.Usuario;
 import com.haroldo.minhasfinancas.service.LancamentoService;
 import com.haroldo.minhasfinancas.service.UsuarioService;
@@ -85,5 +87,128 @@ public class UsuarioResourceTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("email").value(usuarioAutenticado.getEmail()));
 
     }
+
+    @Test
+    public void deveRetornarBadRequestAoObterErroDeAutenticacao() throws Exception {
+
+        //Cenario
+
+        // BACK-END
+        String email = "usuario@email.com";
+        String senha = "123";
+
+        // Representa o json
+        UsuarioDTO dto = UsuarioDTO.builder()
+                .nome("Fulano 1")
+                .email(email)
+                .senha(senha)
+                .build();
+
+        Mockito.when(service.autenticar(email, senha)).thenThrow(ErroAutenticacaoException.class);
+
+        //Converter o dto em json para enviar na requisicao
+        String json = new ObjectMapper().writeValueAsString(dto);
+
+        // FRONT-END
+
+        //Execucao e verificacao
+
+        //Para criar uma requisicao do tipo POST
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .post(API.concat("/autenticar"))
+                .accept(JSON)
+                .contentType(JSON)
+                .content(json);
+
+        mvc.perform(request)
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+
+    }
+
+    @Test
+    public void deveCriarUmNovoUsuario() throws Exception {
+
+        //Cenario
+
+        // BACK-END
+        String email = "usuario@email.com";
+        String senha = "123";
+
+        // Representa o objeto
+        Usuario usuario = Usuario.builder()
+                .id(1L)
+                .nome("Fulano 1")
+                .email(email)
+                .senha(senha)
+                .build();
+
+        // Representa o json
+        UsuarioDTO dto = UsuarioDTO.builder()
+                .nome("Fulano 1")
+                .email(email)
+                .senha(senha)
+                .build();
+
+        Mockito.when(service.salvarUsuario(Mockito.any(Usuario.class))).thenReturn(usuario);
+
+        //Converter o dto em json para enviar na requisicao
+        String json = new ObjectMapper().writeValueAsString(dto);
+
+        // FRONT-END
+
+        //Execucao e verificacao
+
+        //Para criar uma requisicao do tipo POST
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .post(API)
+                .accept(JSON)
+                .contentType(JSON)
+                .content(json);
+
+        mvc.perform(request)
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("id").value(usuario.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("nome").value(usuario.getNome()))
+                .andExpect(MockMvcResultMatchers.jsonPath("email").value(usuario.getEmail()));
+
+    }
+
+    @Test
+    public void deveRetornarBadRequestAoTentarCriarUmUsuarioInvalido() throws Exception {
+
+        //Cenario
+
+        // BACK-END
+        String email = "usuario@email.com";
+        String senha = "123";
+
+        // Representa o json para request front-end
+        UsuarioDTO dto = UsuarioDTO.builder()
+                .nome("Fulano 1")
+                .email(email)
+                .senha(senha)
+                .build();
+
+        Mockito.when(service.salvarUsuario(Mockito.any(Usuario.class))).thenThrow(RegraNegocioException.class);
+
+        //Converter o dto em json para enviar na requisicao
+        String json = new ObjectMapper().writeValueAsString(dto);
+
+        // FRONT-END
+
+        //Execucao e verificacao
+
+        //Para criar uma requisicao do tipo POST
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .post(API)
+                .accept(JSON)
+                .contentType(JSON)
+                .content(json);
+
+        mvc.perform(request)
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+
+    }
+
 
 }
